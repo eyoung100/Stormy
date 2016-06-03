2e9f6a94b16f70b38f38a6af47e10ebce07cdfeb
eyoung100 <ecyoung@mygrande.net>
2016-06-03 15:04:28 -0500

    Initial commit

---
+++ app/src/main/java/com/carterswebs/stormy/CurrentWeather.java
@@ -0,0 +1,10 @@
+package com.carterswebs.stormy;
+
+/**
+ * Created by ecyoung on 6/3/2016.
+ * Holds and Manipulates Current
+ * Weather Data from Forcast.io
+ */
+public class CurrentWeather {
+    
+}
